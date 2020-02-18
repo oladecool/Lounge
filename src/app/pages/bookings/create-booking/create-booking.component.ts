7480cb9fd5536c449dae127b84d4a22c021afc86
Dzifa Atsutse <oladecoolsm>
2020-02-18 14:58:13 +0000

    changes made

--- src/app/pages/bookings/create-booking/create-booking.component.ts
+++ src/app/pages/bookings/create-booking/create-booking.component.ts
@@ -11,7 +11,7 @@ import { NgForm } from '@angular/forms';
 export class CreateBookingComponent implements OnInit {
   @Input () selectedPlace: Place;
   @Input () selectedMode: 'select' | 'random';
-  @ViewChild('f') form: NgForm;
+  @ViewChild('f', { static: true }) form: NgForm;
   startDate: string;
   endDate: string;
 
